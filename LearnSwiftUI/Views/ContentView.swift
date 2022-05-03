147b762090ad8df2e632b8bc2ed45ed46ca46c3d
Erick <erick.pei@qq.com>
2022-05-03 15:07:45 +0800

    feat: SwiftUI Essentials - Building Lists and Navigation

--- LearnSwiftUI/Views/ContentView.swift
+++ LearnSwiftUI/Views/ContentView.swift
@@ -9,47 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("test")
-                    .font(.title)
-                    .foregroundColor(Color.black)
-                HStack {
-                    Text("1111")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("2222")
-                        .font(.subheadline)
-                }
-                
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-                    .font(.body)
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
 
