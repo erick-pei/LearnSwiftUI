baf7d4f1396d4e0456fd1a91a143de80b9ebe05e
Erick <erick.pei@qq.com>
2022-05-03 13:58:43 +0800

    feat: SwiftUI Essentials - Create and Combining Views

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("test")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                HStack {
+                    Text("1111")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("2222")
+                        .font(.subheadline)
+                }
+                
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                    .font(.body)
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
--- Shared/LearnSwiftUIApp.swift
+++ Shared/LearnSwiftUIApp.swift
@@ -7,9 +7,9 @@
 
 import SwiftUI
 
-@main
-struct LearnSwiftUIApp: App {
-    var body: some Scene {
+@main   // main表示程序的入口
+struct LearnSwiftUIApp: App {       // app的生命周期
+    var body: some Scene {  // 返回一个或者多个scenes
         WindowGroup {
             ContentView()
         }
