6916b01c87f18a6b65de3685035f69a1d1e89ef7
Erick <erick.pei@qq.com>
2022-05-03 14:22:18 +0800

    feat: 更新代码结构

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by erick on 2022/5/3.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
